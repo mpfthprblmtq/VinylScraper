18ad9bb5cb7228a8e40299932dacf58c17ec6d59
mpfthprblmtq <1000#Banana>
2021-04-25 20:33:50 -0500

    initial commit, monolith main.py file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # VinylScraper
+
+### What is it
+A scraping bot used to get the newest reddit posts on r/VinylReleases.
+
+### How to use it
+Follow the instructions laid out [here](https://towardsdatascience.com/scraping-reddit-data-1c0af3040768) in the "Getting Started" section.  When you have the client_id, secret, and name, throw those values in the `default_app_info.txt` for the client_id, client_secret, and user_agent, respectively.
+
+You'll need to create an email that you can send emails from.  You can follow [these steps](https://realpython.com/python-send-email/#option-1-setting-up-a-gmail-account-for-development) to create a gmail account for development.
+Once you have that email created, fill the `default_app_info.txt` file with that information.  `sender_email` should be where the emails are coming from, so the email you just created.  `receiving_email` should be wherever you want these emails sent to. `sender_email_password` is the plaintext password for the email account you just created.
+
+Now that your `default_app_info.txt` file has all the information it needs, rename that file to `app_info.txt` so the bot can read it.
+
+Finally, fill the keywords.txt file with keywords you want to search for in the reddit posts.  Each keyword should be on its own line.
---
+++ main.py
@@ -0,0 +1,176 @@
+# imports
+import smtplib
+import ssl
+import time
+import logging
+import praw
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+# class used to store the keyword and time found for alerting
+class Found:
+    def __init__(self, keyword, time_found):
+        self.keyword = keyword
+        self.time_found = time_found
+
+
+# globals
+app_info = []
+keyword_list = []
+already_alerted = []
+SLEEP_INTERVAL_S = 300
+ALERT_COOLDOWN_S = 3600
+
+# logging config
+LOGGING_LEVEL = logging.INFO
+FORMAT = '[%(asctime)s] %(levelname)-7s %(message)s'
+logging.basicConfig(level=LOGGING_LEVEL, format=FORMAT)
+
+
+# cleans the string
+def clean_string(s, start_index):
+    s = s.replace('\n', '')
+    s = s[start_index:len(s)]
+    return s
+
+
+# reads in the app info
+def read_app_info():
+    global app_info  # get the global
+
+    f = open('app_info.txt', 'r')
+    client_id = clean_string(f.readline(), 10)
+    client_secret = clean_string(f.readline(), 14)
+    user_agent = clean_string(f.readline(), 11)
+    sender_email = clean_string(f.readline(), 13)
+    receiving_email = clean_string(f.readline(), 16)
+    sender_email_password = clean_string(f.readline(), 22)
+    app_info = [client_id, client_secret, user_agent, sender_email, receiving_email, sender_email_password]
+
+
+# reads in the keywords.txt file to get all the words to look out for
+def read_keywords():
+    global keyword_list  # get the global
+
+    f = open('keywords.txt', 'r')
+    for line in f:
+        keyword_list.append(clean_string(line, 0))
+
+
+# actually makes the call to reddit
+def make_call(reddit, subreddit):
+    return reddit.subreddit(subreddit).new(limit=10)
+
+
+# looks for any keyword in the title of the post
+def find_keyword(title, keywords):
+    for keyword in keywords:
+        if keyword in title:
+            return keyword
+
+
+def has_already_alerted(keyword):
+    global already_alerted
+    for x in already_alerted:
+        if x.keyword == keyword:
+            return x
+    return None
+
+
+# checks to see if we've already alerted
+def should_alert(keyword):
+    global already_alerted
+    x = has_already_alerted(keyword)    # check to see if we've already sent an alert on this
+    if x is not None:
+        # we have already alerted, check if we're in the cooldown
+        if time.time() - x.time_found > ALERT_COOLDOWN_S:
+            # we are outside of the cooldown period
+            already_alerted.remove(x)
+            return True
+        else:
+            return False
+    else:
+        # keyword wasn't in the already_alerted array, we should alert
+        return True
+
+
+# sends the email
+def send_email(keyword, reddit_post):
+    port = 465  # For SSL
+    password = app_info[5]
+    sender_email = app_info[3]
+    receiver_email = app_info[4]
+    message = MIMEMultipart("alternative")
+    message["Subject"] = f"VinylScraper found a keyword: {keyword}"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    text = f"""\
+VinylScraper is doing its job!  Found a post:
+
+Post title: {reddit_post.title}
+
+Url in post: {reddit_post.url}"""
+
+    # Turn message into plain/html MIMEText object
+    plain_text = MIMEText(text, "plain")
+
+    # Add plain-text part to MIMEMultipart message
+    message.attach(plain_text)
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+
+def get_uptime(start_time):
+    uptime_seconds = time.time() - start_time
+    uptime_h = uptime_seconds // 3600
+    uptime_m = uptime_seconds // 60
+    uptime_s = uptime_seconds - (uptime_m * 60)
+    return str(int(uptime_h)) + "h " + str(int(uptime_m)) + "m " + str(int(uptime_s)) + "s"
+
+
+def main():
+    # populate globals
+    read_app_info()
+    read_keywords()
+    global already_alerted
+
+    # create reddit instance
+    reddit = praw.Reddit(
+        client_id=app_info[0],
+        client_secret=app_info[1],
+        user_agent=app_info[2])
+
+    # get the start time
+    start_time = time.time()
+
+    # do the loop de loop
+    while True:
+        try:
+            posts = make_call(reddit, 'VinylReleases')
+            for post in posts:
+                found_keyword = find_keyword(post.title, keyword_list)
+
+                # check to see if we found a match
+                if found_keyword is not None:
+                    if should_alert(found_keyword):
+                        logging.info(f"Match found ({found_keyword}), sending email!")
+                        send_email(found_keyword, post)
+                        already_alerted.append(Found(found_keyword, time.time()))
+
+            # sleepy sleep
+            logging.info(f"{get_uptime(start_time).ljust(15)} : Sleeping for {SLEEP_INTERVAL_S} seconds...")
+            time.sleep(SLEEP_INTERVAL_S)
+        except Exception as e:
+            logging.error(f'Exception caught in main loop:\n{e}')
+
+
+if __name__ == "__main__":
+    main()
