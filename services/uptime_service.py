43d010c59b9b09b1b26949bdec2f964dc57781a6
mpfthprblmtq <1000#Banana>
2021-09-21 15:48:26 -0500

    fixed uptime_service again, updated gitignore

--- services/uptime_service.py
+++ services/uptime_service.py
@@ -15,11 +15,13 @@ class UptimeService:
         uptime_d = uptime_seconds // 86400
         uptime_h = uptime_seconds // 3600
         uptime_m = uptime_seconds // 60
+        uptime_s = uptime_seconds
         if uptime_d > 0:
             uptime_h = uptime_h - (uptime_d * 24)
         if uptime_h > 0:
             uptime_m = uptime_m - (uptime_h * 60)
+        if uptime_m > 0:
+            uptime_s = uptime_m - (uptime_m * 60)
 
-        uptime_s = uptime_seconds - ((uptime_d * 86400) + (uptime_m * 60) + (uptime_h * 3600))
         return \
             str(int(uptime_d)) + "d " + str(int(uptime_h)) + "h " + str(int(uptime_m)) + "m " + str(int(uptime_s)) + "s"
